49ca63054d28470d666d406e6a01b7a69403ef5d
BlascsokTibor <136988961+Btibor02@users.noreply.github.com>
2025-02-08 19:42:19 +0100

    Menu content WIP

--- home.php
+++ home.php
@@ -12,9 +12,41 @@
             <?php include 'header.php'; ?>
         </div>
         <div class="mainMenu">
+            <h1>Teszt-----------</h1>
         </div>
         <div class="mainContent">
-            <h2>test</h2>
+            <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "bookstore";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+                
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT author, title, category, summary, price FROM books";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo "<div class='card'>
+                                <img class='cardImg' src='placeholder.png' alt='Book Cover' />
+                                <div class='cardData'>
+                                    <h4> " . $row["author"]. ": " . $row["title"]. "</h4>
+                                    <p> Category: " . $row["category"]." </p>
+                                    <p> " . $row["summary"]." </p>
+                                    <p> " . $row["price"]." SEK</p>
+                                </div>
+                            </div>";
+                    }
+                } else {
+                echo "0 results";
+                }
+                $conn->close();
+            ?>
         </div>
         <div class="footer">
             <?php include 'footer.php'; ?>
